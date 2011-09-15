fd42c43afcaaf0b1883eb924863729d65cf96676
machozhao <machozhao@gmail.com>
2011-09-15 01:02:09 +0000

    Fix db2bp process issue

--- WebSEAL-Log-Tools-Project/webseal-log-fetcher-assembly/src/runtime/bin/clear_xml_evts.sql
+++ WebSEAL-Log-Tools-Project/webseal-log-fetcher-assembly/src/runtime/bin/clear_xml_evts.sql
@@ -2,3 +2,4 @@ connect to eventxml user db2admin using SGMsysadmin001;
 select count(*) from cei_t_xml00;
 delete from cei_t_xml00 where (TIMESTAMP('1970-01-01','00.00.00') + (creation_time_utc/1000) seconds) + 3 days < current_timestamp;
 select count(*) from cei_t_xml00;
+terminate;
